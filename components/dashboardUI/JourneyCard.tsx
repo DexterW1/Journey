8a24928e49965c78781c8f894b351c57b1345374
DexterW1 <56940477+DexterW1@users.noreply.github.com>
2024-07-25 21:43:49 -0700

    removed padding for journey cards

--- components/dashboardUI/JourneyCard.tsx
+++ components/dashboardUI/JourneyCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Card, CardBody, CardHeader } from "@nextui-org/react";
 export default function JourneyCards({ journey, selected }: any) {
   return (
-    <Card className="bg-primary p-2 transition-colors duration-1000 ease-in-out hover:bg-primaryLight">
+    <Card className="bg-primary p-1 transition-colors duration-1000 ease-in-out hover:bg-primaryLight">
       <CardBody>
         <h1 className="text-white">{journey.title}</h1>
       </CardBody>
