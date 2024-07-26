1b7cde3db881fd7e5ea20b5cbc543cce59925e42
DexterW1 <56940477+DexterW1@users.noreply.github.com>
2024-07-25 17:08:30 -0700

    added animations to adding and deleting journeys

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -43,20 +43,26 @@ export default function Dashboard({ user, journey }: any) {
           <div className="order-2 flex flex-col gap-4 rounded-xl bg-cardBackground p-4 lg:order-1">
             <p className="text-large text-textPrimary">My Journeys</p>
             <Divider />
-            {journey.map((journey: any) => (
-              <motion.div
-                onClick={() => setSelectedJourney(journey)}
-                key={journey.id}
-                className="cursor-pointer"
-              >
-                <JourneyCard key={journey.id} journey={journey} />
-              </motion.div>
-            ))}
+            <AnimatePresence>
+              {journey.map((journey: any) => (
+                <motion.div
+                  onClick={() => setSelectedJourney(journey)}
+                  key={journey.id}
+                  className="cursor-pointer"
+                  initial={{ opacity: 0, x: -150 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: -150 }}
+                  transition={{ duration: 1.5 }}
+                >
+                  <JourneyCard key={journey.id} journey={journey} />
+                </motion.div>
+              ))}
+            </AnimatePresence>
             {/* Add new Journey Button */}
             {journey.length < 5 && (
               <div
                 onClick={onOpen}
-                className="flex cursor-pointer flex-row items-center justify-center rounded-xl border-2 border-dashed border-primary py-10"
+                className="flex cursor-pointer flex-row items-center justify-center rounded-xl border-2 border-dashed border-primary py-5"
               >
                 <p className="text-3xl text-primary">+</p>
               </div>
